d4c7cad8087af87749556b9550603a95f7973e8b
william-cass-wright <william.cass.wright@gmail.com>
2023-07-16 19:22:55 -0700

    cleanup

--- mgmt/app.py
+++ mgmt/app.py
@@ -33,6 +33,7 @@ def upload(filename: str, compression: Optional[str] = "gzip") -> None:
     target_path = cwd / target
     files_created = []
     skip_files = [".DS_Store"]
+
     try:
         if target == "all":
             echo("uploading all media objects to S3")
@@ -73,6 +74,7 @@ def search(keyword: str) -> None:
     echo(f"\nSearching `{location}` for keyword `{keyword}`...")
     local_matches = [file for file in local_files if file_mgmt.keyword_in_string(keyword, file)]
     s3_matches = [file for file in s3_keys if file_mgmt.keyword_in_string(keyword, file)]
+
     if len(local_matches + s3_matches) >= 1:
         echo("at least one match found\n")
         echo("Local File Matches")
@@ -85,33 +87,40 @@ def search(keyword: str) -> None:
         table.add_column("Object Key", style="magenta")
         table.add_column("Restored Status")
         doptions = {}
+
         for i, file_name in enumerate(s3_matches):
             try:
                 resp = aws.get_obj_head(file_name)
                 storage_class = resp.get("StorageClass", "STANDARD")
                 last_modified = resp.get("LastModified", "")
                 restored_status = resp.get("Restore")
+
                 if restored_status:
                     restored_status = str(restored_status).split("expiry-date=")[-1].replace('"', "")
+
                 table.add_row(str(i), storage_class, str(last_modified).split(" ")[0], file_name, str(restored_status))
                 doptions[i] = file_name
             except Exception as e:
                 echo(f"An error occurred while getting metadata: {e}", err=True)
 
         console.print(table)
+
         if not typer.confirm("Download?", default=False):
             echo("Aborted.")
         else:
             resp = typer.prompt("Which file? [option #]", type=int)
+
             if check_selection(resp, list(doptions)):
                 aws.download(object_name=doptions[resp])
                 return
             else:
                 return
+
         if not typer.confirm("Check Status?", default=False):
             echo("Aborted.")
         else:
             resp = typer.prompt("Which file? [option #]", type=int)
+
             if check_selection(resp, list(doptions)):
                 aws.get_obj_head(object_name=doptions[resp])
                 echo(json.dumps(aws.obj_head, indent=4, sort_keys=True, default=str))
@@ -158,6 +167,7 @@ def delete(filename: str) -> None:
     """
     aws.get_obj_head(object_name=filename)
     echo(json.dumps(aws.obj_head, indent=4, sort_keys=True, default=str))
+
     if not typer.confirm("Confirm deletion?", default=False):
         echo("Aborted.")
         return
@@ -187,12 +197,16 @@ def ls(location: Optional[str] = "global") -> None:
     elif location == "s3":
         s3_keys = aws.get_files(location=location)
         local_files = [""]
+
     echo()
     echo("Local Files...")
+
     for obj in local_files:
         echo(obj)
+
     echo()
     echo("Bucket Objects...")
+
     for obj in s3_keys:
         echo(obj)
 
@@ -205,14 +219,18 @@ def write_config(config):
     echo()
     env_vars = {}
     config_dict = config.configs
+
     for _, val in config.keys_dict.items():
         key = val.get("name")
+
         if config_dict:
             res_default = config_dict.get(key)
         else:
             res_default = None
+
         res = typer.prompt(f"{key} ({val.get('note')})", type=str, default=res_default)
         env_vars[key] = res
+
     config.write_env_vars(env_vars)
 
 
@@ -222,13 +240,16 @@ def config() -> None:
     Configures the application
     """
     config = Config()
+
     if config.check_exists():
         config.load_env()
         config.print_current_config()
+
         if config.ask_overwrite():
             write_config(config)
     else:
         write_config(config)
+
     echo("Configuration complete.")
     config.print_current_config()
 
