0b44e6f0595a51b2d46d6749875dcda9af4cfb61
william-cass-wright <william.cass.wright@gmail.com>
2023-09-10 17:53:49 -0700

    fixing tests

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,30 +1,8 @@
-import os
-
-import pytest
-
-# from pytest_mock import mocker
 from typer.testing import CliRunner
 
 from mgmt.app import app
 
 runner = CliRunner()
-
-# def test_upload_all():
-#     with runner.isolated_filesystem():
-#         # Create some test files
-#         os.makedirs("test_dir")
-#         with open("test_dir/file1.txt", "w") as f:
-#             f.write("Test file 1")
-#         with open("test_dir/file2.txt", "w") as f:
-#             f.write("Test file 2")
-
-#         # Run the upload command
-#         result = runner.invoke(app, ["upload", "all"])
-
-#         # Assert the expected output or behavior
-#         assert result.exit_code == 0
-#         assert "uploading all media objects to S3" in result.output
-#         # Add more assertions as needed
 
 
 def test_search():
@@ -42,18 +20,3 @@ def test_search():
         assert result.exit_code == 0
         assert "total matches found" in result.output
         # Add more assertions as needed
-
-
-# def test_upload(mocker):
-#     mock_aws = mocker.patch("mgmt.aws.AwsStorageMgmt")
-#     mock_aws.upload_target.return_value = "test_file.gz"
-#     result = runner.invoke(app, ["upload", "test_file"])
-#     mock_aws.upload_target.assert_called_once_with("test_file")
-#     assert result.exit_code == 0
-
-# def test_download(mocker):
-#     mock_aws = mocker.patch("mgmt.aws.AwsStorageMgmt")
-#     mock_aws.download.return_value = "test_file"
-#     result = runner.invoke(app, ["download", "test_file"])
-#     mock_aws.download.assert_called_once_with("test_file")
-#     assert result.exit_code == 0
